44d6b3c4ac63138ef587decc113fb87edea86ef1
Mosuzzzz <180250518+Mosuzzzz@users.noreply.github.com>
2025-11-24 22:18:19 +0700

    implement dock

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import { Welcome } from "#components/Welcome";
 import { Navbar } from "#components/Navbar";
+import { Dock } from "#components/Dock";
 
 
-export { Welcome, Navbar };
+export { Welcome, Navbar, Dock };
